57ae65810dbf9270c1ecde84ed976dbc3e87a702
Emillilyn <emilly.vavrinu@icloud.com>
2026-05-12 13:07:28 +0200

    Update r_dwssap.sh
    I have added FT_LINE1 and 2 and I have changed the format from the horizontal to vertical to see pipelined commands more clearly

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,14 @@
-cat /etc/passwd | grep -v "#" | sed -n 'n;p' | cut -d ':' -f 1 | rev | sort -r | awk -v L1="$FT_LINE1" -v L2="$FT_LINE2" 'NR >= L1 && NR <= L2' | paste -s -d "," - | sed 's/,/, /g' | sed 's/$/./'
+export FT_LINE1=7
+export FT_LINE2=15    
+
+cat /etc/passwd | \
+    grep -v '\#' | \
+    sed '1!n;d' | \
+    cut -d':' -f1 | \
+    rev | \
+    sort -r | \
+    awk 'NR>= ENVIRON["FT_LINE1"] && NR<= ENVIRON["FT_LINE2"]' | \
+    paste -s -d"," - | \
+    sed 's/,/, /g' | \
+    sed 's/$/./' | \
+    tr -d '\n'
